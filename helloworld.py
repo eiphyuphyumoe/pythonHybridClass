01417a6cb23d4c167b609f68fd7c190871f92a2b
eiphyuphyumoe <eiphyuphyu112@gmail.com>
2019-10-27 18:12:07 +0630

    Tax Caculator

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
 print ("Hello World")
+
+
+charge=10000
+tax=50/1000
+total tax= charge*tax
+total paid= charge+total tax
+totalpaid=?
+
+
+
+>>> charge=15000
+>>> tax=50/1000
+>>> totaltax=charge*tax
+>>> totalpaid=charge+totaltax
+>>> totalpaid
+15750.0
